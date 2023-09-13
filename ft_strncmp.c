3ecb8c4ab51e62594e58b8e35cdcef78d115ae39
kayg3 <144529681+kayg3@users.noreply.github.com>
2023-09-13 21:52:16 +0000

    fixed!

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,15 +17,11 @@ int	ft_strncmp(const char *str1, const char *str2, size_t num)
 	size_t	i;
 
 	i = 0;
-	while (*str1 && *str2 && (i < num))
-	{
-		if (str1[i] != str2[i])
-			return (*(unsigned char *)str1 - *(unsigned char *)str2);
+	if (num == 0)
+		return (0);
+	while (*str1 && *str2 && (i < num - 1))
 		i++;
-	}
-	if (i == num)
-		return 0;
-	return (*(unsigned char *)str1 - *(unsigned char *)str2);
+	return (((unsigned char *)str1)[i] - *((unsigned char *)str2)[i]);
 }
 /*
 int	main(void)
