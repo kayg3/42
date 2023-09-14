1093268f697a2c09468dae4f64f690a2aa07e0a3
kayg3 <144529681+kayg3@users.noreply.github.com>
2023-09-14 16:42:42 +0000

    fixed

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -31,7 +31,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*str;
 	size_t	start;
 	size_t	end;
-	
+
 	if (!s1 || !set)
 		return (0);
 	start = 0;
@@ -52,4 +52,4 @@ int	main(void)
 	printf("\n Base string: %s\n Trimmming: %s", s1, trim);
 	printf("\n Trmmed string: %s", ft_strtrim(s1, trim));
 	return (0);
-}/*
+}*/
