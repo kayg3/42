c38a1b740c8d558f72a987ed10d5ca2fc2a75897
kayg3 <144529681+kayg3@users.noreply.github.com>
2023-09-14 17:29:47 +0000

    !

--- ft_split.c
+++ ft_split.c
@@ -21,9 +21,9 @@ size_t	number_of_words(char const *s, char c)
 	i = 0;
 	while (*s)
 	{
-		if (*s != c && *s)
+		if (*s != c)
 			i++;
-			while (s* != c && *s)
+			while (*s != c && *s)
 				s++;
 		else
 			s++;
