33071c885556e36c3bea0f662a75083b4957b2bb
kayg3 <144529681+kayg3@users.noreply.github.com>
2023-09-14 20:54:26 +0000

    debug

--- ft_substr.c
+++ ft_substr.c
@@ -15,23 +15,16 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
-	size_t	i;
-	size_t	k;
 
-	i = 0;
-	k = 0;
-	str =(char *) malloc (sizeof(*s) * (len + 1));
+	if (!s)
+		return (0);
+	if (ft_strlen(s) < start)
+		len = 0;
+	if (ft_strl(s + start) < len )
+		len = ft_strlen(s + start);
+	str = malloc (sizeof(char) * (len + 1));
 	if (!str)
 		return (0);
-	while (s[i])
-	{
-		if (i >= start && k < len)
-		{
-			str[k] = s[i];
-			k++;			
-		}
-		i++;
-	}
-	str[k] = 0;
+	ft_strlcpy(str, s + start, len + 1)
 	return (str);
 }
